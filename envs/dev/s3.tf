9af134cecd1473a2c8567283ec7c535578bfd6b8
Kuniaki Shimizu <kun432.8d1w@gmail.com>
2023-04-27 23:56:05 +0900

    s3バケット削除するようにした。がたぶん効かない

--- envs/dev/s3.tf
+++ envs/dev/s3.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "sagemaker_notebook_index" {
-  bucket = "sagemaker-notebook-index"
+  bucket        = "sagemaker-notebook-index"
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_versioning" "sagemaker_notebook_index" {
@@ -16,4 +17,14 @@ resource "aws_s3_bucket_public_access_block" "sagemaker_notebook_index" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "sagemaker_notebook_index" {
+  bucket = aws_s3_bucket.sagemaker_notebook_index.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
